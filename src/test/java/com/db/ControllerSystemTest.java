01799f5fbb6b1bbac026b1014d0b7bcd597d6019
esadikov <sadikov.e@gmail.com>
2016-03-02 11:01:35 +0300

    tests

--- src/test/java/com/db/ControllerSystemTest.java
+++ src/test/java/com/db/ControllerSystemTest.java
@@ -1,5 +1,9 @@
 package com.db;
 
+import com.db.domain.StockDAO;
+import com.db.domain.Ticker;
+import com.db.ClientUI;
+
 import com.db.service.BuyOperationException;
 import com.db.service.Controller;
 import com.db.service.SellOperationException;
@@ -11,6 +15,9 @@ import static org.junit.Assert.assertEquals;
 
 public class ControllerSystemTest {
     private Controller controller = new Controller();
+    private StockDAO stockDAO = new StockDAO();
+/*    private ClientUI clientUI = new ClientUI();
+    private String[] args = {""};*/
 
     @Test
     public void shouldDoBuyForOneLotOfExistTicker() throws SQLException, BuyOperationException {
@@ -21,4 +28,41 @@ public class ControllerSystemTest {
     public void shouldGetErrorWhenSellZeroLot() throws SQLException, BuyOperationException, SellOperationException {
         controller.sell("TKR", 0);
     }
+
+    @Test
+    public void sellOperation() throws SQLException, BuyOperationException, SellOperationException {
+        controller.sell("TKR", 1);
+    }
+/*
+    @Test
+    public void buyOperation() throws SQLException, BuyOperationException, SellOperationException {
+        controller.buy("TKR", 0);
+    }
+*/
+    @Test(expected = com.db.service.BuyOperationException.class)
+    public void tickerNotFoundBuy() throws SQLException, BuyOperationException, SellOperationException {
+        controller.buy("TKN", 0);
+    }
+
+    @Test(expected = com.db.service.SellOperationException.class)
+    public void tickerNotFoundSell() throws SQLException, BuyOperationException, SellOperationException {
+        controller.sell("TKN", 0);
+    }
+
+    @Test(expected = java.lang.IllegalArgumentException.class)
+    public void invalidOperation() throws SQLException, BuyOperationException, SellOperationException {
+        stockDAO.placeOrder("TKR", 1, 3);
+    }
+/*
+    @Test(expected = com.db.service.BuyOperationException.class)
+    public void clientUI() throws SQLException, BuyOperationException, SellOperationException {
+        clientUI.main(args);
+    }
+
+    @Test
+    public void getTicker() throws SQLException, BuyOperationException, SellOperationException {
+        Ticker ticker = new Ticker("TK1");
+        ticker.getTickerId();
+    }
+*/
 }
---
+++ src/test/java/com/db/StockDAOTest.java
@@ -0,0 +1,7 @@
+package com.db;
+
+/**
+ * Created by Student on 26.02.2016.
+ */
+public class StockDAOTest {
+}
