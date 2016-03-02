ac990dc0dcdaecce988e29b3124990b3b930c9f8
esadikov <sadikov.e@gmail.com>
2016-03-02 12:34:55 +0300

    added tests

--- src/test/java/com/db/ControllerSystemTest.java
+++ src/test/java/com/db/ControllerSystemTest.java
@@ -1,5 +1,6 @@
 package com.db;
 
+import com.db.dal.TickerDAO;
 import com.db.domain.StockDAO;
 import com.db.domain.Ticker;
 import com.db.ClientUI;
@@ -7,17 +8,20 @@ import com.db.ClientUI;
 import com.db.service.BuyOperationException;
 import com.db.service.Controller;
 import com.db.service.SellOperationException;
+import junit.framework.Assert;
 import org.junit.Test;
 
 import java.sql.SQLException;
 
+import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertEquals;
 
 public class ControllerSystemTest {
     private Controller controller = new Controller();
     private StockDAO stockDAO = new StockDAO();
-/*    private ClientUI clientUI = new ClientUI();
-    private String[] args = {""};*/
+    private ClientUI clientUI = new ClientUI();
+    private TickerDAO tickerDAO = new TickerDAO();
+    private String[] args = {""};
 
     @Test
     public void shouldDoBuyForOneLotOfExistTicker() throws SQLException, BuyOperationException {
@@ -31,14 +35,19 @@ public class ControllerSystemTest {
 
     @Test
     public void sellOperation() throws SQLException, BuyOperationException, SellOperationException {
-        controller.sell("TKR", 1);
+        double testResult = controller.sell("TKR", 1);
+        assertEquals(1.0, testResult, 0.0001);
     }
-/*
     @Test
-    public void buyOperation() throws SQLException, BuyOperationException, SellOperationException {
-        controller.buy("TKR", 0);
+    public void buyOperationCalculation() throws SQLException, BuyOperationException, SellOperationException {
+        double testResult = controller.buy("TKR", 2);
+        assertEquals(.5, testResult, .0001);
     }
-*/
+    @Test(expected = IllegalArgumentException.class)
+    public void buyOperation() throws SQLException, BuyOperationException, SellOperationException {
+        controller.buy("TKR", -1);
+    }
+
     @Test(expected = com.db.service.BuyOperationException.class)
     public void tickerNotFoundBuy() throws SQLException, BuyOperationException, SellOperationException {
         controller.buy("TKN", 0);
@@ -49,11 +58,16 @@ public class ControllerSystemTest {
         controller.sell("TKN", 0);
     }
 
-    @Test(expected = java.lang.IllegalArgumentException.class)
-    public void invalidOperation() throws SQLException, BuyOperationException, SellOperationException {
+    @Test
+    public void invalidOperationText() throws SQLException, BuyOperationException, SellOperationException {
+        try {
         stockDAO.placeOrder("TKR", 1, 3);
+            fail();
+        }catch (IllegalArgumentException e) {
+            assertEquals("Unsupported operation 3",e.getMessage());
+        }
     }
-/*
+
     @Test(expected = com.db.service.BuyOperationException.class)
     public void clientUI() throws SQLException, BuyOperationException, SellOperationException {
         clientUI.main(args);
@@ -62,7 +76,14 @@ public class ControllerSystemTest {
     @Test
     public void getTicker() throws SQLException, BuyOperationException, SellOperationException {
         Ticker ticker = new Ticker("TK1");
-        ticker.getTickerId();
+ //       ticker.getTickerId();
+        String ticker1 = ticker.getTickerId();
+        assertEquals("TK1",ticker1);
     }
-*/
+
+    @Test(expected = SQLException.class)
+    public void noJdbcConnection() throws SQLException, BuyOperationException, SellOperationException {
+        tickerDAO.openConnection("Test");
+    }
+
 }
